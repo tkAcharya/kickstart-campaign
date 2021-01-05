779b30b370d3cf0dbbfd8c9e99b366062ba7629e
tkAcharya <>
2021-01-05 14:24:51 +0530

    Added nested routing

---
+++ pages/campaigns/new.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+class CampaignNew extends Component {
+
+  render() {
+    return <h1> New Campaign Page</h1> ;
+  }
+
+}
+
+export default CampaignNew;
