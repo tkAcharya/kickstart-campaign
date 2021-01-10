e3851e6e54d3ec3145ac7d1f82ab6532d4d29b75
tkAcharya <>
2021-01-10 19:40:19 +0530

    Added finalize and approval

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,18 +1,48 @@
 import React, {Component} from 'react';
-import { Card, Grid , Button } from 'semantic-ui-react'
+import { Card, Grid , Button , Table , Container } from 'semantic-ui-react'
 import Layout from '../../../components/layout';
 import getCampaign from '../../../ethereum/campaign';
 import {Link} from '../../../routes';
+import RequestRow from '../../../components/RequestRow';
 
 class RequestIndex extends Component {
 
   static async getInitialProps(props) {
+
+    const campaign = getCampaign(props.query.address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
+    const approversCount = await campaign.methods.approversCount().call();
+
+    const requests = await Promise.all(
+      Array(parseInt(requestCount)).fill().map((element,index)=> {
+        return campaign.methods.requests(index).call()
+      })
+    );
+
     return {
-      address: props.query.address
+      address: props.query.address,
+      requests,
+      requestCount,
+      approversCount
     };
   }
 
+  renderRow() {
+    return this.props.requests.map((request, index) => {
+      return <RequestRow
+          key={index}
+          id={index}
+          request={request}
+          address={this.props.address}
+          approversCount={this.props.approversCount}
+      />
+    });
+  }
+
   render() {
+
+    //instead of writting table always we can use ES6
+    const {Header,Row,HeaderCell,Body} = Table;
 
     return (
       <Layout>
@@ -22,7 +52,24 @@ class RequestIndex extends Component {
         </a>
       </Link>
         <h3>Request List</h3>
-      </Layout>
+        <Table celled structured>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+          <Body>
+            {this.renderRow()}
+          </Body>
+        </Table>
+        <div> Found requests: {this.props.requestCount}</div>
+        </Layout>
 
     );
   }
